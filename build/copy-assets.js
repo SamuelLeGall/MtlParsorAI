6cacf2864fe0181f58e095d8f3f6f2abdc4d6f0d
SamuelLeGall <samuel.legall.29+github@gmail.com>
2024-10-20 04:40:11 +0200

    chore: build in functionnal if you paste manually the secret key file

---
+++ build/copy-assets.js
@@ -0,0 +1,31 @@
+import { copy } from "fs-extra"; // Install this package first
+import { join, basename, dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const copyStaticAssets = async () => {
+  try {
+    // Define your source and destination directories
+    const sourceDirs = [
+      join(__dirname, "../views"),
+      join(__dirname, "../public"),
+    ];
+    const destDir = join(__dirname, "dist");
+
+    // Copy each directory
+    await Promise.all(
+      sourceDirs.map(async (srcDir) => {
+        const destPath = join(destDir, basename(srcDir));
+        await copy(srcDir, destPath);
+        console.log(`Copied ${srcDir} to ${destPath}`);
+      })
+    );
+
+    console.log("Static assets copied successfully.");
+  } catch (error) {
+    console.error("Error copying static assets:", error);
+  }
+};
+
+copyStaticAssets();
